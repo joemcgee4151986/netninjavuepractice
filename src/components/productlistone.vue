812b5b6f9d380bf5f9ce0b1a750924b0c3ed1962
joemcgee4151986 <joerobertmcgee@gmail.com>
2023-02-06 01:37:05 +0900

    feat: added two pages. Will fix bugs later

---
+++ src/components/productlistone.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="productlistone">
+        <h1>Product Listtwo</h1>
+        <ul>
+            
+        </ul>
+            <li v-for=""></li>
+     
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    
+    data() {
+      return {
+       
+  
+        
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
